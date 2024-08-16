f18560fa3c9ba4ac61ee505aca8b1f944ca855a4
SeptAlfauzan <alfauzansepta@gmail.com>
2024-08-16 23:45:21 +0700

    add: Banner component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,17 @@
-import Image from "next/image";
-import styles from "./page.module.css";
 import { Metadata } from "next";
-import ThreeScene from "@/widgets/three_scene";
 import Scene from "@/widgets/threejs/scene";
+import { Banner } from "@/templates/banner";
+import { Text } from "@chakra-ui/react";
 
 export const metadata: Metadata = {
   title: "Septa Alfauzan - Portfolio",
 };
 
 export default function Home() {
-  return <Scene />;
+  return (
+    <main>
+      <Banner />
+      <Scene />
+    </main>
+  );
 }
