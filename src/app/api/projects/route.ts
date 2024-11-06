a5f6e8baac0342b8b029dc13f28576c5cb2c1e46
SeptAlfauzan <alfauzansepta@gmail.com>
2024-11-06 23:20:53 +0700

    update: comment routes.ts for api/projects

--- src/app/api/projects/route.ts
+++ src/app/api/projects/route.ts
@@ -1,61 +1,58 @@
-import ImageKitHelper from "@/app/helper/imagekit";
-import PrismaHelper from "@/app/helper/prisma";
 import { NextResponse } from "next/server";
-import { pipeline } from "stream";
-import { promisify } from "util";
-const pump = promisify(pipeline);
+// import { pipeline } from "stream";
+// import { promisify } from "util";
+// const pump = promisify(pipeline);
+
 export const runtime = "edge";
 export async function GET() {
-  const prismaHelper: PrismaHelper = PrismaHelper.getInstance();
-  const result = await prismaHelper.getPrismaClient().project.findMany({
-    include: {
-      images: true,
-    },
-  });
-
-  console.dir(result, { depth: 2 });
+  // const prismaHelper: PrismaHelper = PrismaHelper.getInstance();
+  // const result = await prismaHelper.getPrismaClient().project.findMany({
+  //   include: {
+  //     images: true,
+  //   },
+  // });
 
   return NextResponse.json({
-    data: result,
+    data: "result",
   });
 }
 
-export async function POST(req: Request, res: Response) {
-  try {
-    const formData = await req.formData();
+// export async function POST(req: Request, res: Response) {
+//   try {
+//     const formData = await req.formData();
 
-    const bannerImg = formData.getAll("banner_img")[0] as File;
-    const img = formData.getAll("img")[0] as File;
-    const imageKit = ImageKitHelper.getInstance();
+//     const bannerImg = formData.getAll("banner_img")[0] as File;
+//     const img = formData.getAll("img")[0] as File;
+//     const imageKit = ImageKitHelper.getInstance();
 
-    const bannerResult = await imageKit.uploadImage(bannerImg);
-    const imgResult = await imageKit.uploadImage(img);
+//     const bannerResult = await imageKit.uploadImage(bannerImg);
+//     const imgResult = await imageKit.uploadImage(img);
 
-    const dateString = "2023-07-28T12:34:56Z";
+//     const dateString = "2023-07-28T12:34:56Z";
 
-    const prismaHelper: PrismaHelper = PrismaHelper.getInstance();
+//     const prismaHelper: PrismaHelper = PrismaHelper.getInstance();
 
-    const result = await prismaHelper.getPrismaClient().project.create({
-      data: {
-        startedAt: new Date(dateString),
-        finishedAt: new Date(dateString),
-        title: formData.get("title") as string,
-        desc: formData.get("desc") as string,
-        images: {
-          create: [
-            {
-              url: bannerResult.url,
-            },
-            {
-              url: imgResult.url,
-            },
-          ],
-        },
-      },
-    });
+//     const result = await prismaHelper.getPrismaClient().project.create({
+//       data: {
+//         startedAt: new Date(dateString),
+//         finishedAt: new Date(dateString),
+//         title: formData.get("title") as string,
+//         desc: formData.get("desc") as string,
+//         images: {
+//           create: [
+//             {
+//               url: bannerResult.url,
+//             },
+//             {
+//               url: imgResult.url,
+//             },
+//           ],
+//         },
+//       },
+//     });
 
-    return NextResponse.json({ status: "success", data: result });
-  } catch (e) {
-    return NextResponse.json({ status: "fail", message: e });
-  }
-}
+//     return NextResponse.json({ status: "success", data: result });
+//   } catch (e) {
+//     return NextResponse.json({ status: "fail", message: e });
+//   }
+// }
